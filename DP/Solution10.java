3deebeb8d601b820691b92bb11247c860e956d97
prathambhokare <prathambhokare2017@gmail.com>
2026-05-25 22:43:37 +0530

    Number Of Ways To Divide Array Into K Part Such That Each Of Them Having Sum Less Than Four

--- DP/Solution10.java
+++ DP/Solution10.java
@@ -22,8 +22,33 @@ public class Solution10 {
         return ans;
     }
 
+    public static int numberOfWays(int[] arr,int m,int K) {
+        int ans=0;
+        //__number of ways to divide array till index i into k partition and having sum(part/piece)<=m
+        int[][] dp=new int[arr.length][K+1];
+        for (int i=0;i<arr.length;i++) {
+            int sum=0;
+            for (int j=i;j>=0;j--) {
+                sum=sum+arr[j];
+                if (sum>m) {
+                    break;
+                }
+                if (j==0) {
+                    dp[i][1]=dp[i][1]+1;
+                }
+                else {
+                    for (int k=2;k<=K;k++) {
+                        dp[i][k]=dp[i][k]+dp[j-1][k-1];
+                    }
+                }
+            }
+        }
+        ans=dp[arr.length-1][K];
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int ans=numberOfWays(new int[]{1,3,2,1}, 4);
+        int ans=numberOfWays(new int[]{1,3,2,1}, 4,4);
         System.out.println(ans);
     }
 }
