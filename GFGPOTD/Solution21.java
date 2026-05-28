e759b630dc47288523551ed07f2c5f11f577fd59
prathambhokare <prathambhokare2017@gmail.com>
2026-05-29 01:22:07 +0530

    GFGPOTD Count Sorted Digit Groupings

--- GFGPOTD/Solution21.java
+++ GFGPOTD/Solution21.java
@@ -2,30 +2,72 @@ import java.util.*;
 
 public class Solution21 {
     
+    // public static int validGroups(String s) {
+    //     int n = s.length();
+    //     // dp[i][sum] = ways till index i with last group sum = sum
+    //     int[][] dp = new int[n][901];
+    //     for (int i = 0; i < n; i++) {
+    //         // form current group from j -> i
+    //         int currSum = 0;
+    //         for (int j = i; j >= 0; j--) {
+    //             currSum += (s.charAt(j) - '0');
+    //             // first group
+    //             if (j == 0) {
+    //                 dp[i][currSum] += 1;
+    //             }
+    //             else {
+    //                 // previous group sum must be <= current group sum
+    //                 for (int prevSum = 0; prevSum <= currSum; prevSum++) {
+    //                     dp[i][currSum] += dp[j - 1][prevSum];
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     int ans = 0;
+    //     for (int sum = 0; sum <= 900; sum++) {
+    //         ans += dp[n - 1][sum];
+    //     }
+    //     return ans;
+    // }
+
     public static int validGroups(String s) {
         int n = s.length();
-        // dp[i][sum] = ways till index i with last group sum = sum
-        int[][] dp = new int[n][901];
+        // stores:
+        // values[i] => (sum -> count of ways)
+        @SuppressWarnings("unchecked")
+        Map<Integer, Integer>[] values = new Map[n];
         for (int i = 0; i < n; i++) {
-            // form current group from j -> i
-            int currSum = 0;
+            values[i] = new HashMap<>();
+        }
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            // create current group from j -> i
             for (int j = i; j >= 0; j--) {
-                currSum += (s.charAt(j) - '0');
+                sum += (s.charAt(j) - '0');
                 // first group
                 if (j == 0) {
-                    dp[i][currSum] += 1;
+                    values[i].put(sum,
+                            values[i].getOrDefault(sum, 0) + 1);
                 }
                 else {
-                    // previous group sum must be <= current group sum
-                    for (int prevSum = 0; prevSum <= currSum; prevSum++) {
-                        dp[i][currSum] += dp[j - 1][prevSum];
+                    Map<Integer, Integer> prevMap = values[j - 1];
+                    for (Map.Entry<Integer, Integer> entry : prevMap.entrySet()) {
+                        int prevSum = entry.getKey();
+                        int count = entry.getValue();
+                        // non-decreasing condition
+                        if (sum >= prevSum) {
+                            values[i].put(sum,
+                                    values[i].getOrDefault(sum, 0) + count);
+                        }
                     }
                 }
             }
         }
+
         int ans = 0;
-        for (int sum = 0; sum <= 900; sum++) {
-            ans += dp[n - 1][sum];
+        // total ways ending at last index
+        for (int count : values[n - 1].values()) {
+            ans += count;
         }
         return ans;
     }
@@ -35,3 +77,5 @@ public class Solution21 {
         System.out.println(ans);
     }
 }
+
+
