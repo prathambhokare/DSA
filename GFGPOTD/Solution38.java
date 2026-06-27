873a2c59e1f21491a62b2a165ee522b0c4141271
prathambhokare <prathambhokare2017@gmail.com>
2026-06-27 20:23:40 +0530

    GFG POTD Ways to Tile the Floor

--- GFGPOTD/Solution38.java
+++ GFGPOTD/Solution38.java
@@ -4,9 +4,11 @@ public class Solution38 {
     static final int MOD = 1000000007;
     public static Integer[] memo;
     public static int solve(int n, int m) {
+        //when n<m there only one way to place 1*m tiles
         if (n < m) {
             return 1;
         }
+        //when n==m there only two ways to place 1*m tiles
         if (n == m) {
             return 2;
         }
@@ -14,6 +16,9 @@ public class Solution38 {
             return memo[n];
         }
 
+        //depends on
+        // [1.Placed horizontal tile] => currentRow-1
+        // [2.Placed vertical tiles]  => currentRow-columnLength
         return memo[n] =
                 (solve(n - 1, m) + solve(n - m, m)) % MOD;
     }
